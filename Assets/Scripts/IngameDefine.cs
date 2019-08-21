4f82ffe298f14fb3b13c4c5e214e36f43abee765
shootdori <shootdori@linefriends.com>
2019-08-21 16:39:23 +0900

    최종 버젼으로 코드 커밋.

--- Assets/Scripts/IngameDefine.cs
+++ Assets/Scripts/IngameDefine.cs
@@ -6,8 +6,13 @@ public static class IngameDefine {
 	public static int cellHeight = 52;
 
 	//MapValue.
-	public static int maximumHeight = 6;
+	public static int mapHeight = 6;
 	public static int mapRow = 7;
+	public static int maxSpecialBlockCntInBoard = 7;
 
-	public static int maxSpecialBlockCount = 7;
+	public static int normalBlockScore = 100;
+	public static int specialBlockScore = 200;
+
+	public static int maximumMoveCount = 30;
+	public static int specialBlockMissionCount = 8;
 }
